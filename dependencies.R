b8c87d3edbaeb96ce8d83ef22ec416c9e86e97a8
Bert Overduin <bert.overduin@aridhia.com>
2025-02-27 11:22:17 +0000

    Added check to dependencies.R

--- dependencies.R
+++ dependencies.R
@@ -1,4 +1,15 @@
+package_install <- function(x, ...) {
+  for (i in x) {
+    # Check if package is installed
+    if (!require(i, character.only = TRUE)){
+      # If the package could not be loaded then install it
+      install.packages(i)
+    }
+  }
+}
 
-install.packages("shiny")
-install.packages("shinydashboard")
-install.packages("dplyr")
+# Source this script to install all the libraries needed for the app
+
+packages <- c("shiny", "shinydashboard", "dplyr")
+
+package_install(packages)
